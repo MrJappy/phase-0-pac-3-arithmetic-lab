5a5bd6339ed9b87761449798b15ec8044ad02eba
+ <+>
2023-01-11 17:59:51 -0600

    commit tree cedafd50ddfab65246a9e202b125c1f71d5c4d81

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(num) {
+    return num+=2
+};
+function subtract(num) {
+    return num-=2
+};
+function multiply(num) {
+    return num *=2
+};
+function divide(num) {
+    return num/=2
+};
+function add(a,b) {
+    return a + b
+};
+function subtract(a,b) {
+    return a - b
+};
+function multiply(a,b) {
+    return a * b
+};
+function divide(a,b) {
+    return a / b
+};
+function increment(a) {
+    return a+=1
+};
+function decrement(a) {
+    return a-=1
+};
+function makeInt(n) {
+    return parseInt(n, 10)  
+};
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
